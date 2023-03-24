79a9765254d7bab59cfaaa611c32ad906b2565b0
Kazuki Hashimoto <kazukih@tuta.io>
2023-03-25 00:59:13 +0900

    Revert "SystemUI: Fix QS mobile icon disappearing on theme switch"
    This reverts commit f87a2b0100dfa83cb3b0ef80123cc3d27de0f029.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -183,9 +183,6 @@ public class QuickStatusBarHeader extends FrameLayout {
 
     void setIsSingleCarrier(boolean isSingleCarrier) {
         mIsSingleCarrier = isSingleCarrier;
-        if (mIsSingleCarrier) {
-            mIconContainer.removeIgnoredSlots(mRssiIgnoredSlots);
-        }
         updateAlphaAnimator();
     }
 
