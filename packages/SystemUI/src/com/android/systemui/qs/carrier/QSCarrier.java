8334f550d88aaf761cb7dcecc28d6eb431f597b9
Kazuki Hashimoto <kazukih@tuta.io>
2023-03-25 00:59:10 +0900

    Revert "SystemUI: Remove visibility check in setting QSCarrier color"
    This reverts commit db10d4ff7fdb9f6c7e3cb126dc13d7c699401899.

--- packages/SystemUI/src/com/android/systemui/qs/carrier/QSCarrier.java
+++ packages/SystemUI/src/com/android/systemui/qs/carrier/QSCarrier.java
@@ -134,8 +134,11 @@ public class QSCarrier extends LinearLayout {
     }
 
     public void updateColors(ColorStateList colorStateList) {
-        mMobileRoaming.setImageTintList(colorStateList);
-        mMobileSignal.setImageTintList(colorStateList);
+        final boolean visible = !mIsSingleCarrier;
+        if (visible) {
+            mMobileRoaming.setImageTintList(colorStateList);
+            mMobileSignal.setImageTintList(colorStateList);
+        }
     }
 
     @VisibleForTesting
