283f9fdcc77795d24ddbad7d83a881749c3eb6a1
Kazuki H <kazukih0205@gmail.com>
2025-10-29 18:45:01 +0900

    Revert "[BugFix]Fix NPE when pip onAnimationEnd"
    This reverts commit 4b1f33b84d5661def509449659d718e71d5f4af2.

--- libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/StageCoordinator.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/StageCoordinator.java
@@ -1906,7 +1906,6 @@ public class StageCoordinator implements SplitLayout.SplitLayoutHandler,
     }
 
     void getStageBounds(Rect outTopOrLeftBounds, Rect outBottomOrRightBounds) {
-        if (mSplitLayout == null) return;
         outTopOrLeftBounds.set(mSplitLayout.getTopLeftBounds());
         outBottomOrRightBounds.set(mSplitLayout.getBottomRightBounds());
     }
